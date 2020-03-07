7ef541c9cf1b92a64abca0cc2f0c3001fed3980a
tbag <692603114@qq.com>
2020-03-07 23:29:52 +0800

    增强string modify

--- common/shrong_string.hpp
+++ common/shrong_string.hpp
@@ -10,11 +10,10 @@
 #include <regex>
 
 namespace sstr {
-  static std::vector<std::string> split(const std::string &text, const std::string &re) {
-    std::vector<std::string> ret;
-    std::copy(std::sregex_token_iterator(text.begin(), text.end(), std::regex(re), -1),
-              std::sregex_token_iterator(),
-              std::back_inserter(ret));
-    return ret;
+  inline std::vector<std::string> split(const std::string &text, const std::string &delim) {
+    return std::vector<std::string> {
+        std::sregex_token_iterator(text.begin(), text.end(), std::regex{delim}, -1),
+        std::sregex_token_iterator()
+    };
   }
 }
