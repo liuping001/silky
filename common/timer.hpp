c93b8b8a4b1ae7e70f326319a29cf81da05bba21
tbag <692603114@qq.com>
2020-03-12 20:51:14 +0800

    timer repeated

--- common/timer.hpp
+++ common/timer.hpp
@@ -9,35 +9,51 @@
 #include <functional>
 #include <vector>
 #include <memory>
+#include <unordered_set>
 
 class Timer {
  public:
-  using TimeOutTask = std::function<void()>;
+  using TimeOutTask = std::function<bool ()>; // 返回值表示是否继续加入定时器
   struct TimeInfo {
     TimeInfo(TimeOutTask t) : task(std::move(t)){}
     TimeOutTask task;
+    uint32_t interval = 0;
+    uint32_t seq = 0;
   };
-  using TimerId = std::pair<int64_t, std::shared_ptr<TimeInfo>>;
-
+  using TimerId = std::pair<TimeInfo*, uint32_t>;
+  using TimerNode = std::pair<time_t, std::shared_ptr<TimeInfo>>;
   ~Timer() {
     Clear();
   }
-  TimerId AddTimer(int64_t ms, TimeOutTask &&task) {
-    auto ret = timer_set_.emplace(ms, std::make_shared<TimeInfo>(std::move(task)));
-    return *ret.first;
+  TimerId AddTimer(time_t now, time_t interval, TimeOutTask &&task) {
+    auto t = std::make_shared<TimeInfo>(std::move(task));
+    t->interval = interval;
+    t->seq = now / 1000;
+    auto ret = timer_set_.emplace(now + interval, t);
+    return TimerId(t.get(), t->seq);
   }
   void CancelTimer(const TimerId &timer_id) {
-    timer_set_.erase(timer_id);
+    cancel_list_.insert(timer_id);
   }
-  void DoTimeOutTask(int64_t now_ms) {
-    auto end = timer_set_.lower_bound(TimerId(now_ms + 1, nullptr));
+  void DoTimeOutTask(time_t now_ms) {
+    auto end = timer_set_.lower_bound(TimerNode(now_ms + 1, nullptr));
     if (end == timer_set_.begin()) {
       return;
     }
-    std::vector<TimerId > time_out_set(timer_set_.begin(), end);
+    std::vector<TimerNode> time_out_set(timer_set_.begin(), end);
     timer_set_.erase(timer_set_.begin(), end);
     for (auto &item : time_out_set) {
-      item.second->task();
+      if (!cancel_list_.empty()) {
+        auto iter = cancel_list_.find(TimerId(item.second.get(), item.second->seq));
+        if (iter != cancel_list_.end()) {
+          cancel_list_.erase(iter);
+          continue;
+        }
+      }
+      auto b = item.second->task();
+      if (b) {
+        timer_set_.emplace(now_ms + item.second->interval, item.second);
+      }
     }
   }
   size_t TimerSize() {
@@ -47,6 +63,7 @@ class Timer {
     timer_set_.clear();
   }
  private:
-  std::set<TimerId> timer_set_;
+  std::set<TimerNode> timer_set_;
+  std::set<TimerId> cancel_list_;
 };
 
--- test/timer_test.cpp
+++ test/timer_test.cpp
@@ -8,12 +8,36 @@
 
 TEST_F(my_timer) {
   Timer timer;
-  timer.AddTimer(1000, [](){ cout << "hello world\n";});
+  timer.AddTimer(1000, 1, [](){
+    cout << "hello world\n";
+    return false;
+  });
   timer.DoTimeOutTask(1001);
-  timer.AddTimer(1002, []() { cout << "!!!\n";});
-  auto timer_id = timer.AddTimer(1002, []() { cout << "!!!\n";});
+  timer.AddTimer(1002, 1, []() {
+    cout << "!!!\n";
+    return false;
+  });
+  auto timer_id = timer.AddTimer(1002, 1, []() {
+    cout << "!!!\n";
+    return false;
+  });
   timer.CancelTimer(timer_id);
   timer.DoTimeOutTask(1003);
+
+  timer.AddTimer(1003, 1, [](){
+    cout << "timer 1\n";
+    return true;
+  });
+  timer_id = timer.AddTimer(1003, 1, []() {
+    cout << "timer 2\n";
+    return true;
+  });
+  timer.DoTimeOutTask(1004);
+  timer.DoTimeOutTask(1005);
+  timer.DoTimeOutTask(1006);
+  timer.CancelTimer(timer_id);
+  timer.DoTimeOutTask(1007);
+  timer.DoTimeOutTask(1008);
 }
 
 TEST_FINSH
